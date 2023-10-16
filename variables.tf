20ed7cb7ed808505bd96c6b906f284cbc61aab74
jesusAlvarezSwissre <147830164+jesusAlvarezSwissre@users.noreply.github.com>
2023-10-16 16:55:42 +0200

    vars extracted

--- variables.tf
+++ variables.tf
@@ -2,3 +2,44 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+
+variable "ami_filter" {
+    description = "Name Filter Vars"
+    
+    type = object({
+      name  = string
+      owner = string
+    }})
+
+    default = {
+      name = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+      owner = "979382823631" # Bitnami
+    }
+}
+
+variable "environment" {
+  description = "Environment var"
+
+  type = object ({
+    name = string
+    network_prefix = string
+  })
+
+  default = {
+    name = "dev"
+    network_prefix = "10.0"  
+  }
+  
+}
+
+var asg_min_size {
+  description = "minimum instances in cluster"
+
+  default = 1
+}
+
+var asg_max_size {
+  description = "maximum instances in cluster"
+
+  default = 2
+}
