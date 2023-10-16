fc35003ffdddb6e16d59ed444390c6ecd7244eaf
jesusAlvarezSwissre <147830164+jesusAlvarezSwissre@users.noreply.github.com>
2023-10-16 15:20:43 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -83,7 +83,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
-  vpc_id  = module.vpc.public_subnets[0]
+  vpc_id  = module.vpc.vpc_id
   name    = "blog"
   ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
